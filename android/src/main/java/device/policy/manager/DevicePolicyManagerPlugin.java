c718b428f7b2608ec2460b0982975f056d9cec2f
X-SLAYER <slayertn1999@gmail.com>
2022-03-30 00:07:00 +0100

    add native methods

--- android/src/main/java/device/policy/manager/DevicePolicyManagerPlugin.java
+++ android/src/main/java/device/policy/manager/DevicePolicyManagerPlugin.java
@@ -1,5 +1,11 @@
 package device.policy.manager;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -8,31 +14,68 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** DevicePolicyManagerPlugin */
+
 public class DevicePolicyManagerPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "device_policy_manager");
-    channel.setMethodCallHandler(this);
-  }
+    private MethodChannel channel;
+    private static Context appContext;
+    private static final String CHANNEL_TAG = "x.slayer/device_policy_manager";
+    static final int RESULT_ENABLE = -11;
+    DevicePolicyManager deviceManger;
+    ComponentName compName;
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "device_policy_manager");
+        channel.setMethodCallHandler(this);
+        context = flutterPluginBinding.getApplicationContext();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        compName = new ComponentName(appContext, DeviceAdmin.class);
+        deviceManger = (DevicePolicyManager) appContext.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        if (call.method.equals("enable")) {
+            String message = call.argument("package_name").toString();
+            enableAccess(message);
+        } else if (call.method.equals("disable")) {
+            deviceManger.removeActiveAdmin(compName);
+        } else if (call.method.equals("lockScreen")) {
+            boolean active = deviceManger.isAdminActive(compName);
+            if (active) {
+                deviceManger.lockNow();
+                result.success(true);
+            } else {
+                result.error("-1", "You need to enable the Admin Device Features", null);
+            }
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case RESULT_ENABLE:
+                if (resultCode == RESULT_ENABLE) {
+                    Log.d("PERMISSION", "You have enabled the Admin Device features");
+                } else {
+                    Log.d("PERMISSION", "Problem to enable the Admin Device features");
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void enableAccess(String message) {
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName);
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, message);
+        startActivityForResult(intent, RESULT_ENABLE);
+    }
 }
